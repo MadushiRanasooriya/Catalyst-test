053a9d2748f54f061dcc488b582cc1169d408b87
MadushiRanasooriya <smadu1996@gmail.com>
2023-12-12 12:12:15 +1100

    logic task final

--- Logic_Task/foobar.php
+++ Logic_Task/foobar.php
@@ -1,5 +1,6 @@
 <?php
 	for ($i = 1; $i <= 100; $i++) {
+		//check if the ith number is divisible by 3 ot 5
 		if (divide($i, 3) || divide($i, 5)) {
 			if (divide($i, 3)) {
 				echo "foo";
@@ -19,7 +20,7 @@
 	
 	
 	//function to Check if a given number is divisible by a specified divisor
-	function divide($number, $divisor, $string="test") {
+	function divide($number, $divisor) {
 		if ($number % $divisor === 0) {
 			return true;
 		} else {
