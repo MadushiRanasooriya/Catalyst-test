4451b88be0c5788fc567ecbe733bca9423ebd0fc
MadushiRanasooriya <smadu1996@gmail.com>
2023-12-12 11:52:36 +1100

    printing number array

--- Logic_Task/foobar.php
+++ Logic_Task/foobar.php
@@ -1,14 +1,24 @@
 <?php
 	for ($i = 1; $i <= 100; $i++) {
-		
-		echo "$i";
-		
+		if (divide($i, 3) || divide($i, 5)) {
+			if (divide($i, 3)) {
+				echo "foo";
+			}
+			if (divide($i, 5)) {
+				echo "bar";
+			}
+		}
+		else {
+			echo "$i";
+		}
 		if ($i<100) {
 			echo ", ";
 		}
 	} 
 	echo "\n";
 	
+	
+	//function to Check if a given number is divisible by a specified divisor
 	function divide($number, $divisor) {
 		if ($number % $divisor === 0) {
 			return true;
