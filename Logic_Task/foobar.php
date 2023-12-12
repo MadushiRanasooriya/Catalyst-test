2293c093857df32766352029128fa08ad928c69e
MadushiRanasooriya <smadu1996@gmail.com>
2023-12-12 11:23:55 +1100

    divisability

--- Logic_Task/foobar.php
+++ Logic_Task/foobar.php
@@ -1,10 +1,20 @@
 <?php
 	for ($i = 1; $i <= 100; $i++) {
+		
 		echo "$i";
+		
 		if ($i<100) {
 			echo ", ";
 		}
 	} 
 	echo "\n";
+	
+	function divide($number, $divisor) {
+		if ($number % $divisor === 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 	
 ?>
